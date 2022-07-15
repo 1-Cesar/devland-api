522c98a2d1b771ad3ba309d71a500fb6cedbfe01
Alyson Campos <alyson.siqueiracampos@gmail.com>
2022-07-15 10:32:54 -0300

    projeto devland-api adicionado

---
+++ src/main/java/br/com/vemser/devlandapi/DevlandApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vemser.devlandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlandApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vemser/devlandapi/DevlandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vemser.devlandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
