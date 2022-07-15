9f4749c71f291cf815176910e58c947c4acd13c8
Cesar <cesar.desenvolvedor@gmail.com>
2022-07-15 18:13:44 -0300

    script banco de dados

--- src/main/java/br/com/vemser/devlandapi/dto/UsuarioCreateDTO.java
+++ src/main/java/br/com/vemser/devlandapi/dto/UsuarioCreateDTO.java
@@ -1,4 +1,13 @@
 package br.com.vemser.devlandapi.dto;
 
-public class UsuarioCreateDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioCreateDTO extends UsuarioDTO {
+
+    private Integer idUsuario;
 }
--- src/main/java/br/com/vemser/devlandapi/entity/Usuario.java
+++ src/main/java/br/com/vemser/devlandapi/entity/Usuario.java
@@ -1,4 +1,18 @@
 package br.com.vemser.devlandapi.entity;
 
+import br.com.vemser.devlandapi.enums.TipoUsuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+
+    private String nome, email, senha, areaAtuacao, cpfCnpj;
+
+    private TipoUsuario tipoUsuario;
+
+    private Integer idUsuario;
 }
