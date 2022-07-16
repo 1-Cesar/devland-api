77fb7869bee94560bb4010173ddf76689e5bd948
Cesar <cesar.desenvolvedor@gmail.com>
2022-07-16 11:36:20 -0300

    list por id usuario

--- src/main/java/br/com/vemser/devlandapi/controller/UsuarioController.java
+++ src/main/java/br/com/vemser/devlandapi/controller/UsuarioController.java
@@ -22,9 +22,14 @@ public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
 
-    @GetMapping("/listar")
+    @GetMapping
     public ResponseEntity<List<UsuarioDTO>> listarTodos() throws RegraDeNegocioException {
         return ResponseEntity.ok(usuarioService.listar());
+    }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<List<UsuarioDTO>> listarUsuario(@PathVariable("idUsuario") Integer id) throws RegraDeNegocioException {
+        return ResponseEntity.ok(usuarioService.listarUsuario(id));
     }
 
     @PostMapping
