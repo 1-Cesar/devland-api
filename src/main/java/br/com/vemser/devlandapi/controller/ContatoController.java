7926047955672fc62462a695b7ec6ef6b21c8e01
Cesar <cesar.desenvolvedor@gmail.com>
2022-07-17 03:11:34 -0300

    add api responses em contatoController

--- src/main/java/br/com/vemser/devlandapi/controller/ContatoController.java
+++ src/main/java/br/com/vemser/devlandapi/controller/ContatoController.java
@@ -5,6 +5,8 @@ import br.com.vemser.devlandapi.dto.ContatoDTO;
 import br.com.vemser.devlandapi.exceptions.RegraDeNegocioException;
 import br.com.vemser.devlandapi.service.ContatoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,24 +26,52 @@ public class ContatoController {
     private ContatoService contatoService;
 
     @Operation(summary = "listar todos os contatos", description = "recupera todos os contatos do banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna a lista de contatos"),
+                    @ApiResponse(responseCode = "400", description = "Nenhum contato encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<ContatoDTO>> listar() throws RegraDeNegocioException {
         return ResponseEntity.ok(contatoService.listar());
     }
 
     @Operation(summary = "listar contato por id", description = "recupera um contato do banco de dados atraves de seu id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna um contato através de seu id"),
+                    @ApiResponse(responseCode = "400", description = "Nenhum contato encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @GetMapping("/{idContato}")
     public ResponseEntity<List<ContatoDTO>> listarContato(@PathVariable("idContato") Integer id) throws RegraDeNegocioException {
         return ResponseEntity.ok(contatoService.listarContato(id));
     }
 
     @Operation(summary = "listar contato por id do usuario", description = "recupera um contato do banco de dados atraves do id do usuario")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna contato através do id do usuário"),
+                    @ApiResponse(responseCode = "400", description = "Nenhum contato encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @GetMapping("usuario/{idUsuario}")
     public ResponseEntity<List<ContatoDTO>> listarContatoUsuario(@PathVariable("idUsuario") Integer id) throws RegraDeNegocioException {
         return ResponseEntity.ok(contatoService.listarContatoUsuario(id));
     }
 
     @Operation(summary = "criar contato atraves do id do usuario", description = "cria um contato dentro do banco de dados com base no id do usuario")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Adiciona contato através do id do usuário"),
+                    @ApiResponse(responseCode = "400", description = "Nenhum usuário encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PostMapping("/{idPessoa}")
     public ResponseEntity<ContatoCreateDTO> adicionar(@PathVariable("idPessoa") Integer id,
                                                       @Valid @RequestBody ContatoCreateDTO contato) throws RegraDeNegocioException {
@@ -49,6 +79,13 @@ public class ContatoController {
     }
 
     @Operation(summary = "altera um contato por id", description = "altera os registros de um contato no banco de dados atraves de seu id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Modifica um contato através do seu id"),
+                    @ApiResponse(responseCode = "400", description = "Nenhum contato encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PutMapping("/{idContato}")
     public ResponseEntity<ContatoDTO> editar(@PathVariable("idContato") Integer id,
                                              @Valid @RequestBody ContatoDTO contatoAtualizar) throws RegraDeNegocioException {
@@ -56,6 +93,13 @@ public class ContatoController {
     }
 
     @Operation(summary = "deleta contato", description = "deleta um contato do banco de dados atraves de seu id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Deleta um contato através de seu id"),
+                    @ApiResponse(responseCode = "400", description = "Nenhum contato encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @DeleteMapping("/{idContato}")
     public void remover(@PathVariable("idContato") Integer id) throws RegraDeNegocioException {
         contatoService.remover(id);
