8fec78c4a9a31c7adb5f19dc8476997a041f4207
Patricia <patricia.r.cam@gmail.com>
2022-07-29 11:20:28 -0300

    [Feature] Inserido package de Security

---
+++ src/main/java/br/com/vemser/devlandapi/security/SecurityConfiguration.java
@@ -0,0 +1,82 @@
+package br.com.vemser.devlandapi.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final TokenService tokenService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // Desabilitar frameOptions
+        http.headers().frameOptions().disable().and()
+                // Habilitar cors
+                .cors().and()
+                // Desabilitar csrf
+                .csrf().disable()
+
+                // Adicionar regras de requisição
+                .authorizeHttpRequests((authz) -> authz.antMatchers("/", "/auth", "auth/registrar")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+        // Adicionar filtro do token
+        http.addFilterBefore(new TokenAuthenticationFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // FIXME fazer o security ignorar o swagger
+        return (web) -> web.ignoring().antMatchers("/v3/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui/**");
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .exposedHeaders("Authorization");
+            }
+        };
+    }
+
+    //define qual o tipo de criptografia //todo ldap
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //retorna a autenticação do spring
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+}
---
+++ src/main/java/br/com/vemser/devlandapi/security/TokenService.java
@@ -0,0 +1,71 @@
+package br.com.vemser.devlandapi.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private String expiration;
+
+    private final UsuarioService usuarioService;
+
+    //criando um token JWT
+    public String getToken(UsuarioEntity usuarioEntity) {
+
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + Long.valueOf(expiration)); //convertendo para long
+
+        String token = Jwts.builder()
+                .setIssuer("devland-api")
+                .claim(Claims.ID, usuarioEntity.getIdUsuario())
+                .setIssuedAt(now)
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+
+
+        return TokenAuthenticationFilter.BEARER + token;
+    }
+
+    //validar se o token é válido e retornar o usuário se for válido
+    public UsernamePasswordAuthenticationToken isValid(String token) {
+
+        if (token == null) {
+            return null;
+        }
+
+        Claims payload = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token) //tb verifica a expiração
+                .getBody();
+
+        //busca/ recupera o id que está dentro do body (payload) do JWTS
+        Integer idUsuario = payload.get(Claims.ID, Integer.class);
+
+        if (idUsuario != null) {
+
+            //como o jws já sinalizou que a chave é válida, estamos buscando do póprio jwt
+            // e setando no objeto abaixo
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                    new UsernamePasswordAuthenticationToken(idUsuario, null, Collections.emptyList());
+
+            return usernamePasswordAuthenticationToken;
+        }
+        return null;
+
+    }
+}
