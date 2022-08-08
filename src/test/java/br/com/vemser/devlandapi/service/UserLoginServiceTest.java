2e8a6f0b4861327989cfdeb6c9f743c88fc30294
dayvidsonveiga <dayvidsonveiga@gmail.com>
2022-08-08 10:12:46 -0300

    Feat: criado UserLoginServiceTest

---
+++ src/test/java/br/com/vemser/devlandapi/service/UserLoginServiceTest.java
@@ -0,0 +1,48 @@
+package br.com.vemser.devlandapi.service;
+
+import br.com.vemser.devlandapi.repository.UserLoginRepository;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserLoginServiceTest {
+
+    @InjectMocks
+    private UserLoginService userLoginService;
+
+    @Mock
+    private UserLoginRepository userLoginRepository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Before
+    public void init() {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        ReflectionTestUtils.setField(userLoginService, "objectMapper", objectMapper);
+    }
+
+
+}
